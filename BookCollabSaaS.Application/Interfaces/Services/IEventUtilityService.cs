258b92121111c0e0fc5ad5d4b0b9fdbbcb761e3b
Diogo Ribeiro <diogoribeiro698@gmail.com>
2025-04-28 04:06:41 -0300

    fix: update comment to reflect TODO for adding Stripe.net package

--- BookCollabSaaS.Application/Interfaces/Services/IEventUtilityService.cs
+++ BookCollabSaaS.Application/Interfaces/Services/IEventUtilityService.cs
@@ -4,7 +4,7 @@ namespace BookCollabSaaS.Application.Interfaces.Services;
 
 public interface IEventUtilityService
 {
-    // add nuget package Stripe.net
+    // TODO: add nuget package Stripe.net
     Event ConstructEvent(string json, string stripeSignature, string webhookSecret);
 
 }
